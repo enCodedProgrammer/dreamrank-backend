0b06859bf51f87644b56efdb07d81d25e25d1876
Israel Joseph <obalobomi@gmail.com>
2026-03-11 15:17:44 +0100

    disabled paypal coach auto payment

--- server.js
+++ server.js
@@ -537,12 +537,12 @@ const validCreatorAccount = creatorStripeAccountId !== "null" || creatorStripeAc
           payment_method_types: [paymentOption],
 
             // ✅ PLATFORM FEE (your 10%)
-          application_fee_amount: Math.round((amount * 0.10) + (amount * coachFees)),
+          //application_fee_amount: Math.round((amount * 0.10) + (amount * coachFees)),
 
           // ✅ SEND REMAINDER TO COACH
-          transfer_data: {
-            destination: coachStripeAccountId,
-          },
+          //transfer_data: {
+          //  destination: coachStripeAccountId,
+          //},
 
           //payment_method: paymentMethods.id,
           metadata: { 
@@ -627,7 +627,7 @@ const validCreatorAccount = creatorStripeAccountId !== "null" || creatorStripeAc
                 endTime: endTime,
                 username: username,
                 creatorAccountId: validCreatorAccount ? creatorStripeAccountId : "none",
-                creatorCutCents: creatorCutCents
+                creatorCutCents: creatorCutCents ? creatorCutCents : ""
 
             }
       });
